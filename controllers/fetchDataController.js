062d46bc6ca45d39dfb5fabd073bbd190f5d6475
Harshil Solanki <68768901+hbs246@users.noreply.github.com>
2021-07-09 12:16:27 +0530

    Update fetchDataController.js

--- controllers/fetchDataController.js
+++ controllers/fetchDataController.js
@@ -2,100 +2,42 @@ import requests from 'requests';
 import Data from '../models/data';
 const fetchDataController = {
 
-   fetchData(req,res){
+    fetchData(req, res) {
 
-    
+
         requests("https://api.wazirx.com/api/v2/tickers"
-        ).on("data",async (chunk)=>{
+        ).on("data", async (chunk) => {
             const objData = JSON.parse(chunk);
 
-            let btcinr = {
-                name : objData.btcinr.name,
-                last : objData.btcinr.last,
-                sell : objData.btcinr.sell,
-                volume : objData.btcinr.volume,
-                base_unit : objData.btcinr.base_unit,
-            }
-            let xrpinr = {
-                name : objData.xrpinr.name,
-                last : objData.xrpinr.last,
-                sell : objData.xrpinr.sell,
-                volume : objData.xrpinr.volume,
-                base_unit : objData.xrpinr.base_unit,
-            }
-            let ethinr = {
-                name  : objData.ethinr.name,
-                last : objData.ethinr.last,
-                sell : objData.ethinr.sell,
-                volume : objData.ethinr.volume,
-                base_unit : objData.ethinr.base_unit,
-            }
-            let trxinr = {
-                name : objData.trxinr.name,
-                last : objData.trxinr.last,
-                sell : objData.trxinr.sell,
-                volume : objData.trxinr.volume,
-                base_unit : objData.trxinr.base_unit,
-            }
-            let eosinr = {
-                name : objData.eosinr.name,
-                last : objData.eosinr.last,
-                sell : objData.eosinr.sell,
-                volume : objData.eosinr.volume,
-                base_unit : objData.eosinr.base_unit,
-            }
-            let zilinr = {
-                name : objData.zilinr.name,
-                last : objData.zilinr.last,
-                sell : objData.zilinr.sell,
-                volume : objData.zilinr.volume,
-                base_unit : objData.zilinr.base_unit,
-            }
-            let omginr = {
-                name : objData.omginr.name,
-                last : objData.omginr.last,
-                sell : objData.omginr.sell,
-                volume : objData.omginr.volume,
-                base_unit : objData.omginr.base_unit,
-            }
-            let batinr = {
-                name : objData.batinr.name,
-                last : objData.batinr.last,
-                sell : objData.batinr.sell,
-                volume : objData.batinr.volume,
-                base_unit : objData.batinr.base_unit,
-            }
-            let polyinr = {
-                name : objData.polyinr.name,
-                last : objData.polyinr.last,
-                sell : objData.polyinr.sell,
-                volume : objData.polyinr.volume,
-                base_unit : objData.polyinr.base_unit,
-            }
-            let iostinr = {
-                name : objData.iostinr.name,
-                last : objData.iostinr.last,
-                sell : objData.iostinr.sell,
-                volume : objData.iostinr.volume,
-                base_unit : objData.iostinr.base_unit,
-            }
-             
-            // const  = objData;
-            const arrayData = [btcinr , xrpinr , ethinr , trxinr , eosinr , zilinr , batinr , omginr , polyinr , iostinr ];
-            const top10Data = new Data({
-                tags : arrayData
+            // Get Only Values  From Object
+            const ObjData = Object.values(objData);
+
+            // Remove Additional Key From Array of Object
+            ObjData.forEach( data => {
+                 ["quote_unit","low","high","type","open","buy","at"].forEach( d => delete data[d]);
             });
+
+            // Get only top 10 data from array of object;
+            let dataArray = ObjData.slice(0,10);
+          
+
+            // Save Data into Database
             try {
-             const result = await top10Data.save();   
+                const top10Data = new Data({
+                    tags: dataArray
+                });
+                const result = await top10Data.save();
             } catch (error) {
                 return res.send(error);
             }
-            return res.render('layout',{arrayData})
-        }).on("end",(err)=>{
-            if(err)
-              return console.log('connection closed due to error');
-              
-        });      
+            // Send Response to Client
+            return res.render('layout',{dataArray});
+            
+        }).on("end", (err) => {
+            if (err)
+                return console.log('connection closed due to error');
+
+        });
     }
 }
 
